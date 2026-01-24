bc11dc3ca3a641fda2656584deddb21fc22c0b26
Josh Staples <jwstaples2@gmail.com>
2026-01-24 01:52:10 -0500

    create basic command workflow

--- interfaces/discordInterface.ts
+++ interfaces/discordInterface.ts
@@ -1,14 +1,39 @@
-import { Client, Events, GatewayIntentBits } from "discord.js";
+import { Client, Events, GatewayIntentBits, Guild, MessageFlags, REST, Routes, SlashCommandBuilder } from "discord.js";
 
 /** Discord token stored in .env file */
-export const DISCORD_TOKEN: string | undefined = process.env.DISCORD_TOKEN;
+export const DISCORD_TOKEN = process.env.DISCORD_TOKEN;
 
 export const startDiscordClient = () => {
     const client = new Client({intents: [GatewayIntentBits.Guilds]});
 
-    client.once(Events.ClientReady, () => {
+    client.once(Events.ClientReady, async () => {
         console.log("Logged in!");
+        if(client.user) {
+            await Promise.all(client.guilds.cache.map((server: Guild) => {
+                return refreshServerCommands(client.user!.id, server.id);
+            })).then(() => {
+                console.log('All commands refreshed');
+            });
+        }
     })
 
     client.login(DISCORD_TOKEN);
+
+    client.on(Events.InteractionCreate, async (interaction) => {
+        if(interaction.isChatInputCommand()) {
+            await interaction.reply({content: `${interaction.commandName}`, flags: MessageFlags.Ephemeral});
+        }
+    })
+}
+
+export const refreshServerCommands = async (clientId: string, serverId: string) => {
+    const restAPI = new REST().setToken(DISCORD_TOKEN!);
+    const commands: SlashCommandBuilder[] = [];
+    commands.push(new SlashCommandBuilder().setName("yippee").setDescription("yippee"));
+
+    try {
+        await restAPI.put(Routes.applicationGuildCommands(clientId, serverId), {body: commands});
+    } catch(e) {
+        console.error(e);
+    }
 }
