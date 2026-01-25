5c14e85301718e26875e5d342d94b03fde86754f
Josh Staples <jwstaples2@gmail.com>
2026-01-24 23:58:17 -0500

    replace interface with a class

--- index.ts
+++ index.ts
@@ -1,11 +1,34 @@
-import {
-    DISCORD_TOKEN,
-    startDiscordClient,
-} from "./interfaces/discordInterface.ts";
+import { ChatInputCommandInteraction, EmbedBuilder, Events } from "discord.js";
+import { DiscordInterface } from "./interfaces/discordInterface.ts";
+
+const YIPPEE_GIF = new EmbedBuilder().setImage(
+    "https://media.tenor.com/9BbBRWKXoFcAAAAi/autism-creature-tbh-creature.gif",
+);
+
+const isYippeeCommand = (command: ChatInputCommandInteraction): boolean => {
+    return command.commandName == "yippee";
+};
 
 const initialize = (): void => {
-    console.log("hello world", DISCORD_TOKEN);
-    startDiscordClient();
+    const discordInterface = new DiscordInterface();
+    const discordClient = discordInterface.client;
+
+    discordClient.on(Events.InteractionCreate, async (interaction) => {
+        if (interaction.isChatInputCommand() && isYippeeCommand(interaction)) {
+            await interaction.reply({ embeds: [YIPPEE_GIF] });
+        }
+    });
+
+    discordClient.on(Events.MessageCreate, async (message) => {
+        if (message.author.bot) return;
+
+        const yippeePattern: RegExp = /y+i+p+p+e+e+/i;
+        const words = message.content.split(" ");
+
+        if (words.find((word) => yippeePattern.test(word))) {
+            message.reply({ embeds: [YIPPEE_GIF] });
+        }
+    });
 };
 
 // run the actual function that will start the bot
---
+++ interfaces/DiscordInterface.ts
@@ -0,0 +1,81 @@
+import {
+    Client,
+    Events,
+    GatewayIntentBits,
+    Guild,
+    REST,
+    Routes,
+    SlashCommandBuilder,
+} from "discord.js";
+
+export const DISCORD_TOKEN = process.env.DISCORD_TOKEN;
+
+export class DiscordInterface {
+    private _discordClient: Client;
+    private _commands: SlashCommandBuilder[];
+
+    constructor() {
+        this._discordClient = new Client({
+            intents: [
+                GatewayIntentBits.Guilds,
+                GatewayIntentBits.GuildMessages,
+                GatewayIntentBits.MessageContent,
+            ],
+        });
+        this._commands = [
+            new SlashCommandBuilder()
+                .setName("yippee")
+                .setDescription("yippee"),
+        ];
+
+        this._discordClient.on(Events.GuildMemberAdd, (member) => {
+            this._refreshServerCommands(
+                this._discordClient.application!.id,
+                member.guild.id,
+            ).then(() => {
+                console.log(
+                    `Commands added to new server: ${member.guild.name}`,
+                );
+            });
+        });
+
+        this._onReady();
+
+        this._discordClient.login(DISCORD_TOKEN);
+    }
+
+    get client(): Client {
+        return this._discordClient;
+    }
+
+    private async _onReady() {
+        console.log("Logged in!");
+        if (this._discordClient.user) {
+            await Promise.all(
+                this._discordClient.guilds.cache.map((server: Guild) =>
+                    this._refreshServerCommands(
+                        this._discordClient.user!.id,
+                        server.id,
+                    ),
+                ),
+            ).then(() => {
+                console.log("All commands refreshed");
+            });
+        }
+    }
+
+    private async _refreshServerCommands(clientId: string, serverId: string) {
+        const restAPI = new REST().setToken(DISCORD_TOKEN!);
+
+        try {
+            await restAPI.put(
+                Routes.applicationGuildCommands(clientId, serverId),
+                {
+                    body: this._commands,
+                },
+            );
+        } catch (e) {
+            console.error(e);
+        }
+    }
+}
