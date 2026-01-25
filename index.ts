40250cce8bfb47f44c71f9c3bd23453ea3c73109
mattc101 <mcyc926@gmail.com>
2026-01-25 12:33:40 -0500

    add quote skeleton to index.ts
    you already shknow

--- index.ts
+++ index.ts
@@ -1,9 +1,10 @@
 import { DiscordInterface } from "./interfaces/discordInterface.ts";
 import { handleYippee } from "./handlers/yippeeHandler.ts";
-
+//import { handleQuote } from "./handlers/quoteHandler.ts"; //at some point when there is something to handle 
 const initialize = (): void => {
     const discordInterface = new DiscordInterface();
     handleYippee(discordInterface);
+    //handleQuote(discordInterface); //probably? I know nothing
 };
 
 // run the actual function that will start the bot
